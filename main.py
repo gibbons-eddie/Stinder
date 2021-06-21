68ed996ac377f6e7d3217dc51b97feca5c569a73
YuchenXiong <65258248+YuchenXiong@users.noreply.github.com>
2021-06-21 09:11:22 -0400

    have a database created, might have data inserted?

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
-from PySide6.QtWidgets import QApplication, QMainWindow
+from PySide6.QtWidgets import QApplication, QMainWindow, QTableWidgetItem
+from PySide6.QtSql import QSqlDatabase, QSqlQuery
 
 from logo_test_ui import Ui_MainWindow
 
@@ -15,7 +16,6 @@ class MainWindow(QMainWindow):
 
         # PAGE 2
         self.ui.BrowseButton.clicked.connect(lambda: self.ui.stackedWidget.setCurrentWidget(self.ui.BrowsePage))
-
         # PAGE 3
         self.ui.pushButton_3.clicked.connect(lambda: self.ui.stackedWidget.setCurrentWidget(self.ui.ProfilePage))
 
@@ -33,13 +33,64 @@ class MainWindow(QMainWindow):
 
     # @QtCore.Slot()
     # def magic(self):
-        # self.text.setText(random.choice(self.hello))
+    # self.text.setText(random.choice(self.hello))
+
+
+def insert_data():
+    insertDataQuery = QSqlQuery()
+    insertDataQuery.prepare(
+        """
+        INSERT INTO contacts (
+            name,
+            major,
+            class,
+            email
+        )
+        VALUES (?, ?, ?, ?)
+        """
+    )
+
+    data = [
+        ("Joe", "Computer Science", "COP3502", "joe@example.com"),
+        ("Jack", "Sport Management", "CHM2045", "jack@example.com"),
+        ("Lisa", "Data Science", "IUF1000", "lisa@example.com"),
+        ("Allie", "Biology", "CHM2045", "allie@example.com"),
+    ]
+    for name, major, classes, email in data:
+        insertDataQuery.addBindValue(name)
+        insertDataQuery.addBindValue(major)
+        insertDataQuery.addBindValue(classes)
+        insertDataQuery.addBindValue(email)
+        insertDataQuery.exec()
 
 
 if __name__ == "__main__":
     app = QApplication([])
 
+    con = QSqlDatabase.addDatabase("QSQLITE")
+    con.setDatabaseName("users.db")
+
+    if not con.open():
+        print("Database Error: %s" % con.lastError().databaseText())
+        sys.exit(1)
+
+    createTableQuery = QSqlQuery()
+    createTableQuery.exec(
+        """
+        CREATE TABLE contacts (
+            name VARCHAR(40) PRIMARY KEY NOT NULL,
+            major VARCHAR(40) NOT NULL,
+            classes VARCHAR(50),
+            email VARCHAR(40)
+        )
+        """
+    )
+
+    print(con.tables())
+    insert_data()
+
+    print(con.tables())
     widget = MainWindow()
     widget.show()
 
-    sys.exit(app.exec_())
+    sys.exit(app.exec())
