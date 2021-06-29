94c32bed1067bf276f01cbe5f9c5a06938e3d52b
Eddie Gibbons <gibbons.eddie@ufl.edu>
2021-06-28 22:14:01 -0400

    fixes for classifiers

--- setup.py
+++ setup.py
@@ -14,9 +14,6 @@ setup(
     classifiers=[
         'Development Status :: 3 - Alpha',
 
-        'Intended Audience :: Students',
-        'Topic :: Utilities :: Build Tools',
-
         'License :: OSI Approved :: MIT License',
 
         'Programming Language :: Python :: 3',
