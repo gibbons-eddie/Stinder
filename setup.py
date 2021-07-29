faf281e785f2ba3b1a0cedb05c47bacc20086791
Eddie Gibbons <gibbons.eddie@ufl.edu>
2021-07-28 23:00:08 -0400

    edited packaging settings

--- setup.py
+++ setup.py
@@ -1,15 +1,17 @@
-from setuptools import setup, find_packages
+import setuptools
 import pathlib
 
 here = pathlib.Path(__file__).parent.resolve()
 long_description = (here / 'README.md').read_text(encoding='utf-8')
 
-setup(
+setuptools.setup(
     name='stinder',
-    version='0.0.2',
-    description='Stinder aims to help students find compatible study partners through their major and coursework for optimal study sessions.',
+    version='0.1.9',
+    description='The Stinder project aims to help students find compatible study partners through their major and coursework for optimal study sessions.',
     long_description = long_description,
+    long_description_content_type = 'text/markdown',
     url='https://github.com/gibbons-eddie/Stinder',
+    install_requires=['numpy>=1.21.0', 'PySide6>=6.1.1'],
     author='Eddie Gibbons, Allison Denham, Carlos Echenique, Wyatt Townsend, Yuchen Xiong',
     classifiers=[
         'Development Status :: 3 - Alpha',
@@ -17,12 +19,13 @@ setup(
         'License :: OSI Approved :: MIT License',
 
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3 :: Only',
     ],
-    packages=find_packages(where='src'),
-    python_requires='>=3.8.2, <4'
+    packages=setuptools.find_namespace_packages(include=['stinder', 'stinder.*']),
+    package_data={'':['*.db', '*.otf', '*.qrc', '*.png', '*.jpg']},
+    python_requires='>=3.8.2',
+    entry_points={
+        'console_scripts': [
+            'Stinder = stinder.__main__:main',
+        ]
+    }
 )
